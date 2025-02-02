363aa711bf44dd77adcd793f743a974119dc43ce
Brigitte Parnham <brigitte.parnham@gmail.com>
2025-02-02 20:24:16 +0000

    creating redirect project

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+		strict: false
+	}),
+    paths: {
+      base: '/discover-ANGUILLA'
+    },
+  },
 };
 
 export default config;
+
